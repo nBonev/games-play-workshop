795d3206b4840590af8ff7fb6622ed80e32ff8d2
Nikolay <nbonev5@gmail.com>
2025-03-25 15:57:56 +0200

    Empty games screen

--- client/src/components/game-catalog/GameCatalog.jsx
+++ client/src/components/game-catalog/GameCatalog.jsx
@@ -16,10 +16,10 @@ export default function GameCatalog() {
 
         <section id="catalog-page">
             <h1>All Games</h1>
-            {games.map(game => <GameCatalogItem key={game._id} {...game} />)}
-            
-            {/* <!-- Display paragraph: If there is no games  --> */}
-            <h3 className="no-articles">No articles yet</h3>
+            {games.length > 0 
+                ? games.map(game => <GameCatalogItem key={game._id} {...game} />)
+                : <h3 className="no-articles">No articles yet</h3>
+            }
         </section>
     )
 }
