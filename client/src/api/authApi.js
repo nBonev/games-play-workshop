b27e61f2f57ba9392b25493bde233d11e96e71bb
Nikolay <nbonev5@gmail.com>
2025-04-02 12:53:02 +0300

    Add user register

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,24 +1,35 @@
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
 import requester from "../utils/requester";
 
 const baseUrl = 'http://localhost:3030/users';
 
 export const useLogin = () => {
-    const abortRef = useRef(new AbortController());
+    // const abortRef = useRef(new AbortController());
     
-    const login = async (email, password) => {
-        const result = await requester.post(`${baseUrl}/login`, { email, password }, {signal: abortRef.current.signal });
+    const login = async (email, password) => 
+        requester.post(
+            `${baseUrl}/login`,
+             { email, password },
+            //  {signal: abortRef.current.signal }
+        );
 
-        return result;
-    }
+    // useEffect(() => {
+    //     const abortController = abortRef.current;
 
-    useEffect(() => {
-        const abortController = abortRef.current;
-
-        return () => abortController.abort();
-    }, []);
+    //     return () => abortController.abort();
+    // }, []);
 
     return {
         login,
     };
+}
+
+export const useRegister = () => {
+
+    const register = (email, password) => 
+        requester.post(`${baseUrl}/register`, {email, password})
+
+    return {
+        register,
+    }
 }
